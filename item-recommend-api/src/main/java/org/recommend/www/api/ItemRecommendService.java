570fff4cf2f8003ff2f010fbcd0c5329223b0591
shaw <shaw.xiao@dianping.com>
2016-08-09 10:28:42 +0800

    first version

---
+++ item-recommend-api/src/main/java/org/recommend/www/api/ItemRecommendService.java
@@ -0,0 +1,13 @@
+package org.recommend.www.api;
+
+import java.util.List;
+
+/**
+ * Created by shawxy on 8/9/16.
+ */
+public interface ItemRecommendService {
+
+
+    public List<String> recommedItem(String source);
+
+}
---
+++ item-recommend-service/src/main/java/org/recommend/www/service/impl/ItemRecommendServiceImpl.java
@@ -0,0 +1,17 @@
+package org.recommend.www.service.impl;
+
+import org.recommend.www.api.ItemRecommendService;
+
+import java.util.List;
+
+/**
+ * Created by shawxy on 8/9/16.
+ */
+public class ItemRecommendServiceImpl implements ItemRecommendService {
+
+    public List<String> recommedItem(String source) {
+
+
+        return null;
+    }
+}
